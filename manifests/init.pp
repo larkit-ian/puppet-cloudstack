fb901d335abe84d21c9ca0f82aa2bd909bdd83bc
Ian Forde <iforde@marketo.com>
2014-12-22 11:21:08 -0800

    moved this back too

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,7 +17,10 @@
 # Sample Usage:
 # This class should not be included directly.  It is called from other modules.
 #
-class cloudstack::base () {
+class cloudstack (
+  $csversion  = '4.2',
+  $setup_repo = true,
+) {
 
   # FIXME - Is this dangerous?  I don't know what it does...
   resources { 'host':
@@ -25,17 +28,26 @@ class cloudstack::base () {
     purge => true,
   }
 
-  host { 'localhost':
-    ensure       => present,
-    ip           => '127.0.0.1',
-    host_aliases => [ $::fqdn, 'localhost.localdomain', $::hostname ],
-  }
+  if $setup_repo == true and $::osfamily == 'RedHat' {
+    yumrepo{ 'cloudstack':
+      baseurl  => "http://cloudstack.apt-get.eu/rhel/${csversion}/",
+      enabled  => '1',
+      gpgcheck => '0'
+      before   => Package['cloudstack-management']
+    }
+
 
   # FIXME - could this be a stanza in /etc/sudoers.d?  Depends on the OS.
   # Answer: make this more OS-sensitive.  Maybe use the sudo module?
   file_line { 'cs_sudo_rule':
     path => '/etc/sudoers',
     line => 'cloud ALL = NOPASSWD : ALL',
+  }
+
+  host { 'localhost':
+    ensure       => present,
+    ip           => '127.0.0.1',
+    host_aliases => [ $::fqdn, 'localhost.localdomain', $::hostname ],
   }
 
   package { 'wget': ensure => present } # Not needed after 2.2.9, see bug 11258
