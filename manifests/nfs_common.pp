efde52099b3d7532cc4b9ec35a23ef894007fc73
Ian Forde <iforde@marketo.com>
2014-12-22 10:16:15 -0800

    rename, use parms
    - more changes coming

--- manifests/nfs_common.pp
+++ manifests/nfs_common.pp
@@ -1,36 +1,50 @@
-# Class: cloudstack::nfs-common
 #
-# this subclass provides NFS for primary and secondary storage
+## Class: cloudstack::nfs_common
+#
+# This subclass provides NFS for primary and secondary storage
 # on a single machine. this is not production quality - but useful
 # for a POC/demo/dev/test environment.
 # you will either want to significantly alter or use your own nfs class
-class cloudstack::nfs-common {
+#
+class cloudstack::nfs_common (
+  $cs_pri_storage_nfs_server = '192.168.203.176',
+  $cs_pri_storage_mnt_point  = '/primary',
+  $cs_sec_storage_nfs_server = '192.168.203.176',
+  $cs_sec_storage_mnt_point  = '/secondary',
+  $system_tmplt_dl_cmd       = '/usr/lib64/cloud/agent/scripts/storage/secondary/cloud-install-sys-tmplt',
+  $sysvm_url_kvm             = 'http://download.cloud.com/releases/2.2.0/systemvm.qcow2.bz2',
+  $sysvm_url_xen             = 'http://download.cloud.com/releases/2.2.0/systemvm.vhd.bz2'
+) {
 
   include cloudstack
 
   package {'nfs-utils':
     ensure => present
   }
-
   service {'nfs':
     ensure    => running,
     enable    => true,
     hasstatus => true,
     require   => [ Service[rpcbind], File['/primary'], File['/secondary'] ],
   }
-
   service {'rpcbind':
     ensure    => running,
     enable    => true,
     hasstatus => true,
   }
   file {'/primary':
+    path   => $cs_pri_storage_mnt_point,
     ensure => directory,
     mode   => '0777',
+    owner  => 'root',
+    group  => 'root'
   }
   file {'/secondary':
+    path   => $cs_sec_storage_mnt_point,
     ensure => directory,
     mode   => '0777',
+    owner  => 'root',
+    group  => 'root'
   }
   file {'/etc/sysconfig/nfs':
     source => 'puppet:///modules/cloudstack/nfs',
@@ -38,7 +52,7 @@ class cloudstack::nfs-common {
   }
 
   file {'/etc/exports':
-    source => 'puppet:///modules/cloudstack/exports',
+    source => template('cloudstack/exports.erb'),
     notify => Service[nfs],
   }
 
