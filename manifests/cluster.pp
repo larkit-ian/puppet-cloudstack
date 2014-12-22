72079066427e10fe29999a84afd40c055cb71cce
Ian Forde <iforde@marketo.com>
2014-12-22 10:00:17 -0800

    Some indenting, fix require
    - so we know when it’s safe to apply CS constructs… more work upcoming

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -22,29 +22,29 @@ define cloudstack::cluster(
   $podid,
   $clustertype = 'CloudManaged',
   $hypervisor = $hvtype,
-  ) {
-    #### NEED TO VERIFY THAT ZONEID AND PODID ARE VALID!
-    $teststring_zone = inline_template( "<%= \"http://localhost:\" +
-                   \"${cloudstack::params::mgmt_port}/?command=listZones&\" +
-                   \"available=true\" %>" )
-    $teststring_pod = inline_template( "<%= \"http://localhost:\" +
-                   \"${cloudstack::params::mgmt_port}/?command=listPods&\" +
-                   \"available=true\" %>" )
-    $teststring_cluster = inline_template( "<%= \"http://localhost:\" +
-                   \"${cloudstack::params::mgmt_port}/?command=listClusters&\" +
-                   \"available=true\" %>" )
-    $reststring = inline_template( "<%= \"http://localhost:\" +
-                   \"${cloudstack::params::mgmt_port}/?command=addCluster&\" +
-                   \"clustername=${name}&clustertype=${clustertype}&\" +
-                   \"hypervisor=${hypervisor}&zoneid=${zoneid}&\" +
-                   \"podid=${podid}\" %>" )
+) {
+  #### NEED TO VERIFY THAT ZONEID AND PODID ARE VALID!
+  $teststring_zone = inline_template( "<%= \"http://localhost:\" +
+                 \"${cloudstack::params::mgmt_port}/?command=listZones&\" +
+                 \"available=true\" %>" )
+  $teststring_pod = inline_template( "<%= \"http://localhost:\" +
+                 \"${cloudstack::params::mgmt_port}/?command=listPods&\" +
+                 \"available=true\" %>" )
+  $teststring_cluster = inline_template( "<%= \"http://localhost:\" +
+                 \"${cloudstack::params::mgmt_port}/?command=listClusters&\" +
+                 \"available=true\" %>" )
+  $reststring = inline_template( "<%= \"http://localhost:\" +
+                 \"${cloudstack::params::mgmt_port}/?command=addCluster&\" +
+                 \"clustername=${name}&clustertype=${clustertype}&\" +
+                 \"hypervisor=${hypervisor}&zoneid=${zoneid}&\" +
+                 \"podid=${podid}\" %>" )
 
-    exec { "/usr/bin/curl \'${reststring}\'":
-      onlyif  => [
-        "/usr/bin/curl \'${teststring_zone}\' | grep ${zoneid}",
-        "/usr/bin/curl \'${teststring_pod}\' | grep ${podid}",
-        "/usr/bin/curl \'${teststring_cluster}\' | grep -v ${cluster}"
-      ],
-      require => Exec[ 'cloud_setup_databases' ],
-    }
+  exec { "/usr/bin/curl \'${reststring}\'":
+    onlyif  => [
+      "/usr/bin/curl \'${teststring_zone}\' | grep ${zoneid}",
+      "/usr/bin/curl \'${teststring_pod}\' | grep ${podid}",
+      "/usr/bin/curl \'${teststring_cluster}\' | grep -v ${cluster}"
+    ],
+    require => Anchor['cloudstack::mgmt::anchor_dbsetup_end']
+  }
 }
