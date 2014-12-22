bf76befdb87333bc68a66745e285b8efd24500b4
Ian Forde <iforde@marketo.com>
2014-12-22 12:45:08 -0800

    change tests for now

--- tests/init.pp
+++ tests/init.pp
@@ -1,18 +1,24 @@
-include cloudstack::mgmt
-
-cloudstack::zone { 'zone1': } ->
-
-cloudstack::pod { 'pod1':
-  gateway => '192.168.203.1',
-  netmask => '255.255.255.0',
-  startip => '192.168.203.200',
-  endip   => '192.168.203.230',
-  zoneid  => '1',
+#include cloudstack::mgmt
+class { 'cloudstack::mgmt':
+  csversion  => '4.3.1',
+  setup_repo => false,
+  setup_ntp  => true,
+  uses_xen   => true
 }
 
-cloudstack::cluster { 'cluster1':
-  clustertype => 'CloudManaged',
-  hypervisor  => $hvtype,
-  zoneid      => '1',
-  podid       => '1',
-}
+#cloudstack::zone { 'zone1': } ->
+#
+#cloudstack::pod { 'pod1':
+#  gateway => '192.168.203.1',
+#  netmask => '255.255.255.0',
+#  startip => '192.168.203.200',
+#  endip   => '192.168.203.230',
+#  zoneid  => '1',
+#}
+#
+#cloudstack::cluster { 'cluster1':
+#  clustertype => 'CloudManaged',
+#  hypervisor  => $hvtype,
+#  zoneid      => '1',
+#  podid       => '1',
+#}
