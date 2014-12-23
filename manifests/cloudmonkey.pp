5298bae9af1e86921b8337e2fd21bdeeed066c46
Ian Forde <iforde@marketo.com>
2014-12-23 14:43:22 -0800

    document, add pod script
    yes, we’re going to use cloudmonkey for now

--- manifests/cloudmonkey.pp
+++ manifests/cloudmonkey.pp
@@ -4,6 +4,8 @@
 #   operations
 #
 class cloudstack::cloudmonkey {
+
+  # Prerequisites...
   $needed_packages = [ 'readline', 'python-setuptools' ]
 
   package { $needed_packages:
@@ -11,8 +13,21 @@ class cloudstack::cloudmonkey {
     before => Exec['install_cloudmonkey']
   }
 
+  # Install Cloudmonkey itself
   exec { 'install_cloudmonkey':
     command => '/usr/bin/easy_install cloudmonkey',
     unless  => '/usr/bin/which cloudmonkey 2>/dev/null'
   }
+
+  # Utility scripts, since cloudstack has object IDs that are
+  # a real pain to capture/consume via Puppet...
+  file { '/usr/local/bin/cm_createpod.sh':
+    ensure => present,
+    mode   => '0700',
+    owner  => 'root',
+    group  => 'root',
+    # FIXME:  SELinux labels needed here.
+    source => 'puppet:///modules/cloudstack/cm_create_pod.sh',
+    require => Exec['install_cloudmonkey']
+  } 
 }
