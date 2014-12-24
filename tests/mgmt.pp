32a3f6d5710143c4a4078c673178476359dbaf3a
Ian Forde <iforde@marketo.com>
2014-12-23 18:06:16 -0800

    better test

--- tests/mgmt.pp
+++ tests/mgmt.pp
@@ -1,24 +1,26 @@
 #include cloudstack::mgmt
 class { 'cloudstack::mgmt':
-  csversion  => '4.3',
+  csversion  => '4.4',
   setup_repo => true,
   setup_ntp  => true,
   uses_xen   => true
 }
 
-cloudstack::zone { 'zone1': } ->
+cloudstack::zone { 'zone1': }
+cloudstack::zone { 'zone2': networkdomain => 'foo2.com', networktype => 'Basic' }
+cloudstack::zone { 'zone3': networkdomain => 'foo3.com', networktype => 'Advanced' }
 
 cloudstack::pod { 'pod1':
-  gateway => '192.168.203.1',
-  netmask => '255.255.255.0',
-  startip => '192.168.203.200',
-  endip   => '192.168.203.230',
-  zoneid  => '1',
-} ->
+  gateway  => '192.168.203.1',
+  netmask  => '255.255.255.0',
+  startip  => '192.168.203.200',
+  endip    => '192.168.203.230',
+  zonename => 'zone1'
+}
 
 cloudstack::cluster { 'cluster1':
   clustertype => 'CloudManaged',
-  hypervisor  => $hvtype,
-  zoneid      => '1',
-  podid       => '1',
+  hypervisor  => 'XenServer',
+  zonename    => 'zone1',
+  podname     => 'pod1'
 }
