4e15d077bcf3a6fa17fda5aef8ca97f6ca35d3f1
Ian Forde <iforde@marketo.com>
2014-12-28 03:57:38 -0800

    reorg
    - using a pattern of variables, validation, resources, and dependencies
    - factored out dependencies into section at the bottom

--- manifests/cloudmonkey.pp
+++ manifests/cloudmonkey.pp
@@ -25,48 +25,41 @@
 #
 class cloudstack::cloudmonkey {
 
+  # Variables
+
   $needed_packages = [ 'readline', 'python-setuptools', 'python-pip' ]
+  $list_cluster = $::cloudstack::params::list_cluster_cmd
+  $create_cluster = $::cloudstack::params::create_cluster_cmd
+  $list_pod = $::cloudstack::params::list_pod_cmd
+  $create_pod = $::cloudstack::params::create_pod_cmd
 
-  # Prerequisites...
+  # Resources
+
   if $::osfamily == 'RedHat' {
-    package { 'python-boto':
-      ensure => absent,
-    } ->
-    package { 'python-requests':
-      ensure => absent,
-      before => Exec['install_cloudmonkey']
-    }
+    package { 'python-boto': ensure => absent }
+    package { 'python-requests': ensure => absent }
   }
 
-  package { $needed_packages:
-    ensure => installed,
-    before => Exec['install_cloudmonkey']
-  }
+  package { $needed_packages: ensure => installed }
 
-  # Install Cloudmonkey itself
   exec { 'install_cloudmonkey':
     command => '/usr/bin/pip install cloudmonkey',
     unless  => '/usr/bin/which cloudmonkey 2>/dev/null'
   }
 
-  # Utility scripts, since cloudstack has object IDs that are
-  # a real pain to capture/consume via Puppet...
+  #   Utility scripts, since cloudstack has object IDs that are
+  #   a real pain to capture/consume via Puppet...
 
   File {
     ensure  => present,
     mode    => '0700',
     owner   => 'root',
-    group   => 'root',
-    # FIXME 1:  SELinux labels needed here.
-    require => Exec['install_cloudmonkey']
+    group   => 'root'
+  #   FIXME:  SELinux labels needed here.
   }
 
-  # FIXME 2:  We shouldn't need these scripts at all.  We should
-  # be making REST API calls.  But they're painful to make...
-  $list_cluster = $::cloudstack::params::list_cluster_cmd
-  $create_cluster = $::cloudstack::params::create_cluster_cmd
-  $list_pod = $::cloudstack::params::list_pod_cmd
-  $create_pod = $::cloudstack::params::create_pod_cmd
+  #   FIXME:  We shouldn't need these scripts at all.  We should
+  #   be making REST API calls.  But they're painful to make...
   file {
     "/usr/local/bin/${list_pod}":
       source => "puppet:///modules/cloudstack/${list_pod}";
@@ -77,4 +70,20 @@ class cloudstack::cloudmonkey {
     "/usr/local/bin/${create_cluster}":
       source => "puppet:///modules/cloudstack/${create_cluster}";
   }
+
+  # Finally, our dependencies...
+
+  if $::osfamily == 'RedHat' {
+    Package['python-boto'] ->
+      Package['python-requests'] ->
+      Exec['install_cloudmonkey']
+  }
+  Package[$needed_packages] ->
+    Exec['install_cloudmonkey'] ->
+    File[
+      "/usr/local/bin/${list_pod}",
+      "/usr/local/bin/${create_pod}",
+      "/usr/local/bin/${list_cluster}",
+      "/usr/local/bin/${create_cluster}"
+    ]
 }
