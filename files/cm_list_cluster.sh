12e3934124af3ba674e0e27738c4c2e183b1c7c1
Ian Forde <iforde@marketo.com>
2014-12-30 17:12:53 -0800

    fix another cloudmonkey bug
    - add comments
    - Cloudmonkey 5.3.0 and 5.3.1 (20141230-master) both report success on
    partial object name matches.  Implementing a workaround…

--- files/cm_list_cluster.sh
+++ files/cm_list_cluster.sh
@@ -9,11 +9,17 @@ clustername=$3
 
 zoneid=$(/usr/bin/cloudmonkey list zones name=${zonename} filter=id)
 podid=$(/usr/bin/cloudmonkey list pods name=${podname} zoneid=${zoneid} filter=id)
-clusterexists=$(/usr/bin/cloudmonkey list clusters name=${clustername} podid=${podid} zoneid=${zoneid} filter=id)
-if [ a"${clusterexists}" != "a" ]; then
+
+# The grep at the end of this is due to a potential cloudmonkey regex bug that makes
+# it report partial name matches on objects, unfortunately.  This behavior exists in
+# Cloudmonkey 5.3.0 AND 5.3.1 (as of 20141230)
+
+clusterexists=$(/usr/bin/cloudmonkey list clusters name=${clustername} podid=${podid} zoneid=${zoneid} filter=id,name | grep -q "^name = ${clustername}$")
+ccstatus=$?
+
+if [ ${ccstatus} -eq 0 ]; then
 	# Cluster exists.  We're done here.
 	exit 0
 else
 	exit 1
 fi
-
--- files/cm_list_pod.sh
+++ files/cm_list_pod.sh
@@ -7,8 +7,14 @@ zonename=$1
 podname=$2
 
 zoneid=$(/usr/bin/cloudmonkey list zones name=${zonename} filter=id)
-podexists=$(/usr/bin/cloudmonkey list pods name=${podname} zoneid=${zoneid} filter=id)
-if [ a"${podexists}" != "a" ]; then
+
+# The grep at the end of this is due to a potential cloudmonkey regex bug that makes
+# it report partial name matches on objects, unfortunately.  This behavior exists in
+# Cloudmonkey 5.3.0 AND 5.3.1 (as of 20141230)
+
+podexists=$(/usr/bin/cloudmonkey list pods name=${podname} zoneid=${zoneid} filter=id,name | grep -q "^name = ${podname}$")
+ccstatus=$?
+if [ ${ccstatus} -eq 0 ]; then
 	# Pod exists.  We're done here.
 	exit 0
 else
