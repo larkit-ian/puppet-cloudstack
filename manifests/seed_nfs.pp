8902f709f5bfa8c9ea7801611717a5f6dba469af
Ian Forde <iforde@marketo.com>
2015-01-04 23:10:18 -0800

    - add override for system VM url - formatting updates - rewrite command for creating secondary storage to read the new object id files (yes - I know I said I wasn’t going to use them) - small code refactor allowing the URL to be overridden - because we’re now a defined resource, we can’t inherit.  We have to include cloudstack::params - fix missing path attributes in execs - THIS DEFINED RESOURCE IS STILL COMPLETELY UNTESTED AT THIS POINT.

--- manifests/seed_nfs.pp
+++ manifests/seed_nfs.pp
@@ -1,39 +1,44 @@
 #
 # == Defined resource type: cloudstack::seed_nfs
 #
-#   This class will add NFS-based secondary storage to a Cloudstack zone and seed it with
-#   with system template VMs.
+#   This class will add NFS-based secondary storage to a
+#   Cloudstack zone and seed it with system template VMs.
 #
 # == Parameters
 #
 # == Sample Usage
 #
-define cloudstack::seed_nfs inherits cloudstack::params (
+define cloudstack::seed_nfs (
   $zonename,
   $cs_sec_storage_nfs_url,
   $hvtype,
+  $sysvm_url = 'UNDEF',
 ) {
 
   # Variables
 
-  $cs_sec_storage_name = $name
-  $mgmt_port           = $::cloudstack::mgmt_port
-  $system_tmplt_dl_cmd = '/usr/share/cloudstack-common/scripts/storage/secondary/cloud-install-sys-tmpl'
-  $sysvm_url_kvm       = $::cloudstack::params::sysvm_url_kvm
-  $sysvm_url_xen       = $::cloudstack::params::sysvm_url_xen
-  $ospath              = $::cloudstack::params::ospath
-  $hypervisortypes_small     = $::cloudstack::params::hypervisortypes_small
+  $cs_sec_storage_name   = $name
+  $mgmt_port             = $::cloudstack::mgmt_port
+  $system_tmplt_dl_cmd   = '/usr/share/cloudstack-common/scripts/storage/secondary/cloud-install-sys-tmpl'
+  $sysvm_url_kvm         = $::cloudstack::params::sysvm_url_kvm
+  $sysvm_url_xen         = $::cloudstack::params::sysvm_url_xen
+  $ospath                = $::cloudstack::params::ospath
+  $hypervisortypes_small = $::cloudstack::params::hypervisortypes_small
 
-  $zoneid = inline_template("curl \'http://localhost:<% @mgmt_port %>/?command=listZones&name=<% @zonename %>&response=default | xgrep -s \'zone:name/<% @zonename %>/\' | grep \'<id>\' | sed -e \'s/<[^>]*>//g\' | awk \'{print $1}\'")
-  $chk_sec_storage_cmd = "curl \'http://localhost:${mgmt_port}/?command=listImageStores&zoneid=${zoneid}&response=default | xgrep -s \'imagestore:zonename/${zonename}/\' | grep \'<id>\' | sed -e \'s/<[^>]*>//g\' | awk \'{print $1}\'"
-  $add_sec_storage_cmd = "curl \'http://localhost:${mgmt_port}/?command=addSecondaryStorage&url=nfs://${cs_sec_storage_nfs_url}&zoneid=${zoneid}"
+  #$chk_sec_storage_cmd = "curl \'http://localhost:${mgmt_port}/?command=listImageStores&zoneid=${zoneid}&response=default | xgrep -s \'imagestore:zonename/${zonename}/\' | grep \'<id>\' | sed -e \'s/<[^>]*>//g\' | awk \'{print \$1}\'"
+  $chk_sec_storage_cmd = "curl \'http://localhost:${mgmt_port}/?command=listImageStores&zoneid=`cat /var/tmp/cs_zoneid__${zonename}`&response=default | xgrep -s \'imagestore:zonename/${zonename}/\' | grep \'<id>\' | sed -e \'s/<[^>]*>//g\' | awk \'{print \$1}\'"
+  $add_sec_storage_cmd = "curl \'http://localhost:${mgmt_port}/?command=addSecondaryStorage&url=nfs://${cs_sec_storage_nfs_url}&zoneid=`cat /var/tmp/cs_zoneid__${zonename}`"
 
   $secondary_mnt_base = '/mnt/secondary'
 
-  $sysvm_url = $hvtype ? {
-    'xenserver' => $sysvm_url_xen,
-    'kvm'       => $sysvm_url_kvm,
-    default     => $sysvm_url_kvm
+  if $sysvm_url == 'UNDEF' {
+    $sysvm_url_r = $hvtype ? {
+      'xenserver' => $sysvm_url_xen,
+      'kvm'       => $sysvm_url_kvm,
+      default     => $sysvm_url_kvm
+    }
+  } else {
+    $sysvm_url_r = $sysvm_url
   }
 
   # Validation
@@ -45,6 +50,7 @@ define cloudstack::seed_nfs inherits cloudstack::params (
   # Resources
 
   include ::cloudstack
+  include ::cloudstack::params
   include ::cloudstack::cloudmonkey
 
   # This will need to move out of here and into common...
@@ -63,12 +69,18 @@ define cloudstack::seed_nfs inherits cloudstack::params (
   }
   exec { "cs_add_sec_storage_to_zone__${zonename}":
     command => $add_sec_storage_cmd,
-    unless => $chk_sec_storage_cmd,
+    unless  => $chk_sec_storage_cmd,
+    path    => $ospath
   }
+
   exec { "cs_seed_zone__${zonename}":
+  #
+  #   FIXME:  This resource may take a while to run.
+  #
     command => "${system_tmplt_dl_cmd} -m ${secondary_mnt_base}/zone_${zonename} -u ${sysvm_url} -h ${hvtype} -F ; touch /var/lib/cloud/ssvm",
     # FIXME: Is this a good check?
-    unless  => 'test -f /var/lib/cloud/ssvm'  # FIXME:  This resource may take a while to run.
+    creates => '/var/lib/cloud/ssvm',
+    path    => $ospath
   }
 
   # Dependencies
